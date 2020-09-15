a5b2905edd858a6db45043e39698a64bc8b0f08a
Sam Evans <evansst@icloud.com>
2020-09-15 10:14:43 -0600

    Done.

--- 091520.js
+++ 091520.js
@@ -1,3 +1,17 @@
-const each = (array, callback) => {
-  
+const array = [1,2,3,4,5,6,7]
+
+const map = (array, callback) => {
+  const returnArray = [];
+  for(let element of array) {
+    returnArray.push(callback(element))
+  }
+  return returnArray;
 }
+
+
+const timesTwo = (element) => {
+  return element * 2;
+}
+
+console.log(array.map(el => timesTwo(el)))
+console.log(map(array, timesTwo))
